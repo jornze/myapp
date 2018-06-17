20a7d3414f3fb290e4f43fa7f45a88f4363aa651
wangziqiang <740990616@qq.com>
2018-06-18 00:10:17 +0800

    link mysql

--- src/components/find/find.vue
+++ src/components/find/find.vue
@@ -1,7 +1,39 @@
 <template>
 	<div ref='find'>
-		find
+		<template v-for='item in data'>
+			<p>{{item.age}}-{{item.name}}-{{item.professional}}</p>
+		</template>
 	</div>
 </template>
-<script></script>
+<script>
+export default{
+	data(){
+		return{
+			data:{
+				type:Object,
+				required:true
+			}
+		}
+	},
+	mounted(){
+		this.$nextTick(function(){
+			this.getdata();
+		})
+	},
+	methods:{
+		getdata(){
+			this.$http.get('/nodejson/123').then(res=>{
+			
+				this.data=res.data.data;
+				console.log(this.data);
+			
+			})
+			.catch(err=>{
+				console.log('get123',err)
+			})
+		}
+		
+	}
+}
+</script>
 <style></style>
